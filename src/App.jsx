822e4c2b40782abd3e5ba08bae45c6ce1e56466d
seanka <and.seanka@gmail.com>
2023-12-19 13:05:09 +0700

    Added sub Navigation inside SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List</p>} />
+          <Route path="cities" element={<p>List</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
+
 function AppNav() {
-  return <div></div>;
+  return (
+    <div className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default AppNav;
