932112266d9bc9fb8be9fa3157b759857a982d67
seanka <and.seanka@gmail.com>
2023-12-19 12:46:10 +0700

    Fixed app layout UI

---
+++ src/components/Map.jsx
@@ -0,0 +1,7 @@
+import styles from "./Map.module.css";
+
+function Map() {
+  return <div></div>;
+}
+
+export default Map;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear()} by WorldWise Inc.</p>
+      </footer>{" "}
+    </div>
+  );
+}
+
+export default Sidebar;
