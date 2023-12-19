a6f8cce6fdf3e89511d1b48735fe4e93953c74f8
seanka <and.seanka@gmail.com>
2023-12-19 11:12:24 +0700

    Created PageNav component

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+function PageNav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
